ec9a2fdbbde887ef8641f9090bc1b45881a39c15
Gustavo Correia <gustavocorreiadias@gmail.com>
2026-03-17 12:33:48 -0300

    remove loading bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import AppRoutes from "./routes";
-import LoadingBar from "./components/LoadingBar/LoadingBar";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    window.addEventListener("load", () => {
-      setTimeout(() => setIsLoading(false), 500);
-    });
-
-    const timer = setTimeout(() => setIsLoading(false), 5000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <>
-      <LoadingBar isLoading={isLoading} />
-      {!isLoading && <AppRoutes />}
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
